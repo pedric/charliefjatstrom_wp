26531cf1e2c5486e636f9a2cccb51992471830c6
Fredrik Larsson <epost.larsson@gmail.com>
2018-05-26 23:53:59 +0200

    lightbox styled

--- src/javascripts/modules/lightbox.js
+++ src/javascripts/modules/lightbox.js
@@ -1,6 +1,5 @@
 export default class Lightbox {
   constructor(el) {
     this.el = el
-    console.log(el.textContent, '- From the example module')
   }
 }
---
+++ src/javascripts/modules/menu.js
@@ -0,0 +1,8 @@
+export default class Menu {
+  constructor(el) {
+    
+    this.el = el
+    this.menu = document.getElementById('nav-main')
+    this.toast = document.getElementsByClassName('toast')[0]
+  }
+}
